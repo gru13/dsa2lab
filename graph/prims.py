57e86c94ec40206a8d9a70f7a351ee51a43a692f
gru13 <guruprasath1302@gmail.com>
2023-08-19 22:28:23 +0530

    1st working prims

--- graph/prims.py
+++ graph/prims.py
@@ -12,9 +12,6 @@ def prim(G: Graph, vertex):
         for a in visited_vertice:
             cur_edge += G.edgesOf(a, n=True)
 
-        cur_edge = set(cur_edge) - visited_edges
-        cur_edge = list(cur_edge)
-
         cur_edge = list(sorted(cur_edge, key=lambda x:x[-1]))
         while (cur_edge[0][1] in visited_vertice and cur_edge[0][0] in visited_vertice):
             cur_edge.pop(0)
@@ -22,17 +19,14 @@ def prim(G: Graph, vertex):
         v1 = cur_edge[0][0]
         v2 = cur_edge[0][1]
         w = cur_edge[0][-1]
-        if v2 in visited_vertice and v1 in visited_vertice:
-            visited_edges.add((v1,v2,w))
-            visited_edges.add((v2,v1,w))
-        else:
+        visited_edges.add((v1,v2,w))
+        visited_edges.add((v2,v1,w))
+    
+        if not(v2 in visited_vertice and v1 in visited_vertice):
             visited_vertice.add(v2)
             visited_vertice.add(v1)
-            visited_edges.add((v1,v2,w))
-            visited_edges.add((v2,v1,w))
             yield cur_edge[0]
 
-        # print(visited_edges)
 
 
 
